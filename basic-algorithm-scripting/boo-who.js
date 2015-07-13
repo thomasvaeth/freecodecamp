e4c4176f566326e3fb739d4cd0a390368d66846e
Thomas Vaeth <thomas.vaeth@gmail.com>
2015-07-12 21:29:37 -0400

    Removed if statement for typeof operator.

--- basic-algorithm-scripting/boo-who.js
+++ basic-algorithm-scripting/boo-who.js
@@ -1,11 +1,7 @@
 // Check if a value is classified as a boolean primitive.
 // Return true or false.
 function boo(bool) {
-	if (bool === true || bool === false) {
-		return true;
-	} else {
-		return false;
-	}
+	return typeof bool === 'boolean';
 }
 
 boo(null);
