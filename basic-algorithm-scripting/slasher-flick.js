5253cf44a3be5b20c51540886e203776974860e6
Thomas Vaeth <thomas.vaeth@gmail.com>
2015-07-15 00:20:50 -0400

    Removed for loop.

--- basic-algorithm-scripting/slasher-flick.js
+++ basic-algorithm-scripting/slasher-flick.js
@@ -1,9 +1,6 @@
 // Return the remaining elements of an array after chopping off n elements from the head.
 function slasher(arr, howMany) {
-	for (var i = howMany - 1; i >= 0; i--) {
-		arr.shift(i);
-	}
-	return arr;
+	return arr.slice(howMany);
 }
 
 slasher([1, 2, 3], 2);
