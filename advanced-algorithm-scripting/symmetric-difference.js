d61b739ab0e44d24ba79aea8dc1e644bc225d6ae
Thomas Vaeth <thomas.vaeth@gmail.com>
2015-07-22 10:14:40 -0400

    Used .reduce() twice.

--- advanced-algorithm-scripting/symmetric-difference.js
+++ advanced-algorithm-scripting/symmetric-difference.js
@@ -1,13 +1,20 @@
 // Create a function that takes two or more arrays and returns an array of the symmetric difference of the provided arrays.
 function sym(args) {
-	args = Array.prototype.slice.call(arguments);
-	var arr = args.reduce(function(a, b) {
-		return a.concat(b);
-	});
-	var unique = arr.filter(function (e, i, a) {
-		return a.indexOf(e) === i && a.lastIndexOf(e) === i; 
-	});
-	return unique;
+	var arr = Array.prototype.slice.call(arguments);
+	return arr.reduce(function(arrFirst, arrNext) {
+		var one = arrFirst.filter(function(a) {
+			return arrNext.indexOf(a) === -1;
+		});
+		var two = arrNext.filter(function(a) {
+			return arrFirst.indexOf(a) === -1;
+		});
+		return one.concat(two);
+	}).reduce(function(a,b) {
+		if(a.indexOf(b) < 0) {
+			a.push(b);
+		}
+		return a;
+	}, []);
 }
 
 sym([1, 2, 3], [5, 2, 1, 4]);
