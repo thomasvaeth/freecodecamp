98e81c0a206ae8e4ec97284c90093ca07d47b603
Thomas Vaeth <thomas.vaeth@gmail.com>
2015-09-04 21:05:44 -0700

    Added if statement for background images.

--- local-weather/js/weather.js
+++ local-weather/js/weather.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
   $('.btn-celsius').hide();
   $('.btn-fahrenheit').hide();
-})
+});
 
 $.getJSON('http://ip-api.com/json', function(ipAddress) {
 
   $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + ipAddress.lat + '&lon=' + ipAddress.lon, function(forecast) {
-    var kelvin = forecast.main.temp;
-    var celsius = kelvin - 273.15
-    var fahrenheit = celsius * 1.8 + 32
+    var celsius = forecast.main.temp - 273.15;
+    var fahrenheit = celsius * 1.8 + 32;
+    var backgroundPic = forecast.weather[0].main.toLowerCase();
 
     $(document).ready(function() {
+      if (backgroundPic === 'clouds') {
+        $('body').css('background-image', 'url("images/clear.jpg")');
+      } else if (backgroundPic === 'drizzle') {
+        $('body').css('background-image', 'url("images/drizzle.jpg")');
+      } else if (backgroundPic === 'rain') {
+        $('body').css('background-image', 'url("images/rain.jpg")');
+      } else if (backgroundPic === 'thunderstorm') {
+        $('body').css('background-image', 'url("images/thunder.jpg")');
+      } else if (backgroundPic === 'snow') {
+        $('body').css('background-image', 'url("images/snow.jpg")');
+      } else if (backgroundPic === 'atmosphere') {
+        $('body').css('background-image', 'url("images/fog.jpg")');
+      } else {
+        $('body').css('background-image', 'url("images/def.jpg")');
+      }
       $('.information').text('Hello ' + ipAddress.city + ' from Seattle.');
       $('.btn-check').on('click', function() {
         $('.btn-check').hide();
