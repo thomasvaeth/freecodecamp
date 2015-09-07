9ff72b702dd5b804d8a685d141176d8c9a9ee7d8
Thomas Vaeth <thomas.vaeth@gmail.com>
2015-09-07 01:06:58 -0700

    Finished, but repeating too much. It needs to be cleaned up.

--- pomodoro-clock/js/countdown.js
+++ pomodoro-clock/js/countdown.js
@@ -1,60 +1,78 @@
 $(document).ready(function() {
-	$('.task').html(taskTimer + clock);
-	$('.break').html(breakTimer + clock);
-	$('.break, .start, .stop, .reset, .set-break, .add-break, .subtract-break').hide();
+  $('.task').html(taskTimer + clock);
+  $('.break').html('0' + breakTimer + clock);
+  $('.break, .start-task, .start-break, .stop, .reset, .set-break, .add-break, .subtract-break').hide();
 });
 
 var taskTimer = 25;
 var breakTimer = 5;
 var clock = ':00';
 
-$('.set-task').on('click', function() {
-	$('.task, .set-task, .add-task, .subtract-task').hide();
-	$('.break, .set-break, .add-break, .subtract-break').show();
-});
-
 $('.add-task').on('click', function() {
-	taskTimer++;
-	if (taskTimer > 0) {
-		$('.set-task, .subtract-task').removeAttr('disabled');
-	}
-	$('.task').html(taskTimer + clock);
+  taskTimer++;
+  if (taskTimer > 0) {
+    $('.set-task, .subtract-task').removeAttr('disabled');
+  }
+  if (taskTimer < 10) {
+    $('.task').html('0' + taskTimer + clock)
+  } else {
+    $('.task').html(taskTimer + clock);
+  }
 });
 
 $('.subtract-task').on('click', function() {
-	taskTimer--;
-	if (taskTimer === 0) {
-		$('.set-task, .subtract-task').attr('disabled', 'disabled');
-	}
-	$('.task').html(taskTimer + clock);
+  taskTimer--;
+  if (taskTimer === 0) {
+    $('.set-task, .subtract-task').attr('disabled', 'disabled');
+  }
+  if (taskTimer < 10) {
+    $('.task').html('0' + taskTimer + clock)
+  } else {
+    $('.task').html(taskTimer + clock);
+  }
+});
+
+$('.set-task').on('click', function() {
+  $('.task, .set-task, .add-task, .subtract-task').hide();
+  $('.break, .set-break, .add-break, .subtract-break').show();
 });
 
 $('.add-break').on('click', function() {
-	breakTimer++;
-	if (breakTimer > 0) {
-		$('.set-break, .subtract-break').removeAttr('disabled');
-	}
-	$('.break').html(breakTimer + clock);
+  breakTimer++;
+  if (breakTimer > 0) {
+    $('.set-break, .subtract-break').removeAttr('disabled');
+  }
+  if (breakTimer < 10) {
+    $('.break').html('0' + breakTimer + clock)
+  } else {
+    $('.break').html(breakTimer + clock);
+  }
 });
 
 $('.subtract-break').on('click', function() {
-	breakTimer--;
-	if (breakTimer === 0) {
-		$('.set-break, .subtract-break').attr('disabled', 'disabled')
-	}
-	$('.break').html(breakTimer + clock);
+  breakTimer--;
+  if (breakTimer === 0) {
+    $('.set-break, .subtract-break').attr('disabled', 'disabled')
+  }
+  if (breakTimer < 10) {
+    $('.break').html('0' + breakTimer + clock)
+  } else {
+    $('.break').html(breakTimer + clock);
+  }
 });
 
 $('.set-break').on('click', function() {
-	$('.break, .set-break, .add-break, .subtract-break').hide();
-	$('.start, .clock').show();
+  $('.break, .set-break, .add-break, .subtract-break').hide();
+  $('.start-task, .task').show();
 });
 
-var minutesLeft = taskTimer;
+var minutesLeft;
 var secondsLeft = 0;
 var timeSetup;
 
-function countdownClock() {
+function taskClock() {
+  $('.clock, .information').css('color', 'white');
+  $('.information').text('Left in Task');
   secondsLeft--;
   if (minutesLeft < 10 && secondsLeft < 10) {
     $('.clock').html('0' + minutesLeft + ':0' + secondsLeft);
@@ -65,37 +83,79 @@ function countdownClock() {
   } else {
     $('.clock').html(minutesLeft + ':' + secondsLeft);
   }
+  if (minutesLeft < 1) {
+  	$('.clock, .information').css('color', 'red');
+  }
   if (secondsLeft < 0) {
     if (minutesLeft === 0 && secondsLeft < 0) {
+      taskTimer = 0;
       clearInterval(timeSetup);
       $('.clock').html('00:00');
+      $('.stop').hide();
+      $('.start-break').show();
     } else {
       minutesLeft--;
       secondsLeft = 60;
-      countdownClock();
+      taskClock();
     }
   }
 }
 
-$('.start').on('click', function() {
-	minutesLeft = taskTimer;
-	secondsLeft = 0;
-	timeSetup;
-	$('.start').hide();
-	$('.stop').show();
-	timeSetup = setInterval( function() {countdownClock()}, 1000);
-	countdownClock();
+function breakClock() {
+  $('.clock, .information').css('color', 'white');
+  $('.information').text('Left in Break');
+  secondsLeft--;
+  if (minutesLeft < 10 && secondsLeft < 10) {
+    $('.clock').html('0' + minutesLeft + ':0' + secondsLeft);
+  } else if (minutesLeft < 10) {
+    $('.clock').html('0' + minutesLeft + ':' + secondsLeft);
+  } else if (secondsLeft < 10) {
+    $('.clock').html(minutesLeft + ':0' + secondsLeft);
+  } else {
+    $('.clock').html(minutesLeft + ':' + secondsLeft);
+  }
+  if (minutesLeft < 1) {
+  	$('.clock, .information').css('color', 'red');
+  }
+  if (secondsLeft < 0) {
+    if (minutesLeft === 0 && secondsLeft < 0) {
+      taskTimer = 0;
+      clearInterval(timeSetup);
+      $('.clock').html('00:00');
+      $('.stop').text('Reset');
+    } else {
+      minutesLeft--;
+      secondsLeft = 60;
+      breakClock();
+    }
+  }
+}
+
+$('.start-task').on('click', function() {
+  $('.task, .start-task, .subtract-break').hide();
+  $('.clock, .stop').show();
+  timeSetup = setInterval( function() {taskClock()}, 1000);
+  minutesLeft = taskTimer;
+  taskClock();
+});
+
+$('.start-break').on('click', function() {
+  $('.task, .start-break, .subtract-break').hide();
+  $('.clock, .stop').show();
+  timeSetup = setInterval( function() {breakClock()}, 1000);
+  minutesLeft = breakTimer;
+  breakClock();
 });
 
 $('.stop').on('click', function() {
-  $('.stop').hide();
-  $('.task').html(taskTimer + clock);
-  $('.break').html(breakTimer + clock);
+  $('.stop, .clock, .information').hide();
+  $('.stop').text('Stop');
   $('.task, .set-task, .add-task, .subtract-task').show();
-  $('.clock').hide();
   clearInterval(timeSetup);
-  taskTimer;
-  breakTimer;
+  taskTimer = 25;
+  $('.task').html(taskTimer + clock)
+  breakTimer = 5;
+  $('.break').html('0' + breakTimer + clock);
   minutesLeft = taskTimer;
   secondsLeft = 0;
   $('.clock').html('');
