5455076df999f0cfd956ee871e59100b6658211c
Thomas Vaeth <thomas.vaeth@gmail.com>
2015-09-19 12:05:46 -0700

    Syntax.

--- basic-algorithm-scripting/factorialize-a-number.js
+++ basic-algorithm-scripting/factorialize-a-number.js
@@ -1,14 +1,14 @@
 // Return the factorial of the provided integer.
 function factorialize(num) {
-    var total = num;
-    if (num === 0) {
-        return 1;
-    } else {
-	    for (var i = num - 1; i > 0; i--) {
-		    total *= i;
-	    }
-	    return total;
-    }
+	var total = num;
+	if (num === 0) {
+		return 1;
+	} else {
+		for (var i = num - 1; i > 0; i--) {
+			total *= i;
+		}
+		return total;
+	}
 }
 
 factorialize(5, '');
