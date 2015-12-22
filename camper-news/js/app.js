21c175f9043cff997dd3ae6ec1d782de75a3dd6e
thomasvaeth <thomas.vaeth@gmail.com>
2015-12-22 09:18:00 -0800

    Finished setting up Angular.

--- camper-news/js/app.js
+++ camper-news/js/app.js
@@ -1,1 +1,15 @@
 var app = angular.module('CamperNewsApplication', ['ngResource']);
+
+app.factory('Story', ['$resource', function($resource) {
+	return $resource('http://www.freecodecamp.com/news/hot/:id');
+}]);
+
+app.controller('StoryController', ['$scope', 'Story', function($scope, Story) {
+	$scope.stories = [];
+
+	Story.query(function success(data) {
+		$scope.stories = data;
+	}, function error(data) {
+		console.log(data);
+	});
+}]);
