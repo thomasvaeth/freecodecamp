e1955c2b0fa93568305e7bded5f6736de73bd935
thomasvaeth <thomas.vaeth@gmail.com>
2016-01-20 17:28:02 -0800

    Seeing text in textarea.

--- markdown-preview/js/app.js
+++ markdown-preview/js/app.js
@@ -6,20 +6,26 @@ var App = React.createClass({
   }
 });
 
+// var TextInput = React.createClass({
 
+// });
 
 var InputDisplay = React.createClass({
+	input: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Thomas Vaeth](http://freecodecamp.com/thomasvaeth)*',
 	getInitialState: function() {
-		return {input: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](http://freecodecamp.com/hermanfassett)*'};
+		return {input: this.input};
+	},
+	changeInput: function(e) {
+		this.setState({input: e.target.value});
 	},
 	render: function() {
 		return (
 			<div className="row">
 				<div className="small-6 columns">
-					<h1>Hello!</h1>
+					<textarea rows="25" value={this.state.input} onChange={this.changeInput}></textarea>
 				</div>
 				<div className="small-6 columns">
-					<h2>World!</h2>
+					
 				</div>
 			</div>
 		);
