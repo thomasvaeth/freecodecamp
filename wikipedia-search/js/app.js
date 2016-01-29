74cad15eb98d069844fbb33057c2b7e01cac40d5
thomasvaeth <thomas.vaeth@gmail.com>
2016-01-28 21:24:56 -0800

    Added API.

--- wikipedia-search/js/app.js
+++ wikipedia-search/js/app.js
@@ -2,4 +2,16 @@ var app = angular.module('WikipediaApplication', []);
 
 app.controller('WikipediaController', ['$scope', '$http', function($scope, $http) {
 	$scope.results = [];
+
+	$scope.searchWikipedia = function() {
+		var api = 'http://en.wikipedia.org/w/api.php?action=query&generator=search&prop=extracts&exintro&exsentences=1&exlimit=max&explaintext&gsrsearch=';
+		var callback = '&format=json&callback=JSON_CALLBACK';
+		$http.jsonp(api + $scope.searchTerm + callback
+		).success(function(data) {
+			$scope.results = data.query.pages;
+			console.log($scope.results);
+		}).error(function(data) {
+			console.log(data);
+		});
+	}
 }]);
