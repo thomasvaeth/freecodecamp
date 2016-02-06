b119d6f476910b3ffc92788a309e47991470d741
thomasvaeth <thomas.vaeth@gmail.com>
2016-02-06 11:30:25 -0800

    Added some AI.

--- tic-tac-toe/js/tictactoe.js
+++ tic-tac-toe/js/tictactoe.js
@@ -43,7 +43,7 @@ function getSymbol() {
 		rightScore.getElementsByTagName('p')[0].innerHTML = "Player O Wins:";
 		leftScore.getElementsByTagName('p')[0].innerHTML = "Computer X Wins:";
 		visibilePage();
-		computerMove();
+		neverLose();
 	});
 }
 
@@ -55,40 +55,82 @@ function visibilePage() {
 	leftScore.style.visibility = 'visible';
 }
 
-function computerMove() {
-	goComputer = Math.floor(Math.random() * 9);
-	if (gameboard[goComputer] === '') {
-		gameboard[goComputer] = computerSymbol;
-		spaces[goComputer].innerHTML = computerSymbol;
-		if (computerSymbol === 'X') {
-			spaces[goComputer].classList.add('red');
-			turn.innerHTML = "It's O's turn to go.";
-			arrX.push(parseInt(spaces[goComputer].getAttribute('data-num')));
-			clicks++;
-			if (checkForWin(arrX) === true) {
-				turn.innerHTML = "Computer X won! New game. It's X's turn to go.";
-        xWins++;
-        leftScoreSpan.innerHTML = xWins;
-      	clearBoard();
-			}
-		} else {
-			spaces[goComputer].classList.add('blue');
-			turn.innerHTML = "It's X's turn to go.";
-			arrO.push(parseInt(spaces[goComputer].getAttribute('data-num')));
-			clicks++;
-			if (checkForWin(arrO) === true) {
-				turn.innerHTML = "Computer O won! New game. It's X's turn to go.";
-        oWins++;
-        rightScoreSpan.innerHTML = oWins;
-      	clearBoard();
-			}
-		}	
+function computerMove(idx) {
+	if (computerSymbol === 'X') {
+		spaces[idx].classList.add('red');
+		turn.innerHTML = "It's O's turn to go.";
+		arrX.push(parseInt(spaces[idx].getAttribute('data-num')));
+		clicks++;
+		if (checkForWin(arrX) === true) {
+			turn.innerHTML = "Computer X won! New game. It's X's turn to go.";
+      xWins++;
+      leftScoreSpan.innerHTML = xWins;
+    	clearBoard();
+		}
 	} else {
-		if (clicks >= 9) {
-			turn.innerHTML = "The game was a draw. New game. It's X's turn to go.";
-			clearBoard();
+		spaces[idx].classList.add('blue');
+		turn.innerHTML = "It's X's turn to go.";
+		arrO.push(parseInt(spaces[idx].getAttribute('data-num')));
+		clicks++;
+		if (checkForWin(arrO) === true) {
+			turn.innerHTML = "Computer O won! New game. It's X's turn to go.";
+      oWins++;
+      rightScoreSpan.innerHTML = oWins;
+    	clearBoard();
+		}
+	}
+
+	if (clicks >= 9) {
+		turn.innerHTML = "The game was a draw. New game. It's X's turn to go.";
+		clearBoard();
+	}
+}
+
+function neverLose() {
+	if (gameboard[0] === '' && ((gameboard[1] === symbol && gameboard[2] === symbol) || (gameboard[3] === symbol && gameboard[6] === symbol) || (gameboard[4] === symbol && gameboard[8] === symbol))) {
+		gameboard[0] = computerSymbol;
+		spaces[0].innerHTML = computerSymbol;
+		computerMove(0);
+	} else if (gameboard[1] === '' && ((gameboard[0] === symbol && gameboard[2] === symbol) || (gameboard[4] === symbol && gameboard[7] === symbol))) {
+		gameboard[1] = computerSymbol;
+		spaces[1].innerHTML = computerSymbol;
+		computerMove(1);
+	} else if (gameboard[2] === '' && ((gameboard[0] === symbol && gameboard[1] === symbol) || (gameboard[5] === symbol && gameboard[8] === symbol) || (gameboard[4] === symbol && gameboard[6] === symbol))) {
+		gameboard[2] = computerSymbol;
+		spaces[2].innerHTML = computerSymbol;
+		computerMove(2);
+	} else if (gameboard[3] === '' && ((gameboard[4] === symbol && gameboard[5] === symbol) || (gameboard[0] === symbol && gameboard[6] === symbol))) {
+		gameboard[3] = computerSymbol;
+		spaces[3].innerHTML = computerSymbol;
+		computerMove(3);
+	} else if (gameboard[4] === '' && ((gameboard[3] === symbol && gameboard[5] === symbol) || (gameboard[1] === symbol && gameboard[7] === symbol) || (gameboard[0] === symbol && gameboard[8] === symbol) || (gameboard[2] === symbol && gameboard[6] === symbol))) {
+		gameboard[4] = computerSymbol;
+		spaces[4].innerHTML = computerSymbol;
+		computerMove(4);
+	} else if (gameboard[5] === '' && ((gameboard[3] === symbol && gameboard[4] === symbol) || (gameboard[2] === symbol && gameboard[8] === symbol))) {
+		gameboard[5] = computerSymbol;
+		spaces[5].innerHTML = computerSymbol;
+		computerMove(5);
+	} else if (gameboard[6] === '' && ((gameboard[7] === symbol && gameboard[8] === symbol) || (gameboard[0] === symbol && gameboard[3] === symbol) || (gameboard[2] === symbol && gameboard[4] === symbol))) {
+		gameboard[6] = computerSymbol;
+		spaces[6].innerHTML = computerSymbol;
+		computerMove(6);
+	} else if (gameboard[7] === '' && ((gameboard[6] === symbol && gameboard[8] === symbol) || (gameboard[1] === symbol && gameboard[4] === symbol))) {
+		gameboard[7] = computerSymbol;
+		spaces[7].innerHTML = computerSymbol;
+		computerMove(7);
+	} else if (gameboard[8] === '' && ((gameboard[2] === symbol && gameboard[5] === symbol) || (gameboard[6] === symbol && gameboard[7] === symbol) || (gameboard[0] === symbol && gameboard[4] === symbol))) {
+		gameboard[8] = computerSymbol;
+		spaces[8].innerHTML = computerSymbol;
+		computerMove(8);
+	} else {
+		goComputer = Math.floor(Math.random() * 9);
+		if (gameboard[goComputer] === '') {
+			gameboard[goComputer] = computerSymbol;
+			spaces[goComputer].innerHTML = computerSymbol;
+			computerMove(goComputer);
 		} else {
-			computerMove();
+			neverLose();
 		}
 	}
 }
@@ -111,7 +153,7 @@ function emptySpace(event) {
       	clearBoard();
       	return false;
 			}
-			computerMove();
+			neverLose();
 		} else {
 			event.target.classList.add('blue');
 			turn.innerHTML = "It's X's turn to go.";
@@ -124,7 +166,7 @@ function emptySpace(event) {
       	clearBoard();
       	return false;
 			}
-			computerMove();
+			neverLose();
 		}
 		if (clicks >= 9) {
 			turn.innerHTML = "The game was a draw. New game. It's X's turn to go.";
@@ -159,7 +201,7 @@ function clearBoard() {
 	arrO = [];
 	gameboard = ['', '', '', '', '', '', '', '', ''];
 	if (computerSymbol === 'X') {
-		computerMove();
+		neverLose();
 	}
 }
 
