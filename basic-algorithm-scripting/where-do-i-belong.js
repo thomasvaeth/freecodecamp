f62f5d068e47b8b0bce6df7c782876d3e611ba84
thomasvaeth <thomas.vaeth@gmail.com>
2016-05-28 12:43:14 -0700

    Updated old challenge.

--- basic-algorithm-scripting/where-do-i-belong.js
+++ basic-algorithm-scripting/where-do-i-belong.js
@@ -1,15 +1,12 @@
 // Return the lowest index at which a value (second argument) should be inserted into a sorted array (first argument).
-function where(arr, num) {
-  var newArr = [];
-  arr = arr.sort();
+function getIndexToIns(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
   for (var i = 0; i < arr.length; i++) {
-    if (num <= arr[i]) {
-      newArr.push(num);
-      return i;
-    } else {
-      newArr.push(arr[i]);
-    }
+    if (arr[i] >= num) return i;
   }
+  return arr.length; 
 }
 
-where([40, 60], 50);
+getIndexToIns([40, 60], 50);
